cb66a29579940e021536f190d344f11eb263fa40
marjohncornelio <112856637+marjohncornelio@users.noreply.github.com>
2022-11-21 09:12:57 +0800

    deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,7 +74,10 @@ const forgotPassword = async (req, res) => {
 
     const DOMAIN = "surewinmarketplace.tech";
     const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
-    const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
+    const mg = mailgun({
+      apiKey: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_API_BASE_URL,
+    });
     const data = {
       from: "noreply@surewinmarketplace.tech",
       to: email,
