a73379788c233fac9aa0c9891aaf052e13ef4763
marjohncornelio <112856637+marjohncornelio@users.noreply.github.com>
2022-11-27 23:21:25 +0800

    fix reccurring issue

--- app.js
+++ app.js
@@ -27,48 +27,55 @@ app.get("/", (req, res) => {
 });
 //Recurring Payment for Rental and Monthly Billings
 app.post("/recurring-rent", async (req, res) => {
-  const response = await reccur.fetchData();
-  const tenant = await response.tenant;
-  let contract = await response.contract;
-  const Invoice = await response.Invoice;
+  try {
+    const response = await reccur.fetchData();
+    const tenant = await response.tenant;
+    let contract = await response.contract;
+    const Invoice = await response.Invoice;
 
-  let tempcontract = contract.filter((c) => c.rental_frequency === "Daily");
-  tempcontract.forEach((c) => {
-    Invoice.create({
-      tenant_id: c.tenant_id,
-      status: "Unpaid",
-      payment_for: "Rent",
-      amount_to_paid: c.rental_amount,
-      due_date: new Date(),
-      description: "Rental Collection",
-      received: 0,
-      balance: c.rental_amount,
-    });
-  });
-
-  if (
-    new Date() ===
-    new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
-  ) {
-    tempcontract = contract.filter((c) => c.rental_frequency === "Monthly");
+    let tempcontract = contract.filter((c) => c.rental_frequency === "Daily");
     tempcontract.forEach((c) => {
       Invoice.create({
         tenant_id: c.tenant_id,
         status: "Unpaid",
         payment_for: "Rent",
-        amount_to_paid:
-          c.rental_amount *
-          new Date(
-            new Date().getFullYear(),
-            new Date().getMonth() + 1,
-            0
-          ).getDate(),
+        amount_to_paid: c.rental_amount,
         due_date: new Date(),
         description: "Rental Collection",
         received: 0,
         balance: c.rental_amount,
       });
     });
+
+    if (
+      new Date() ===
+      new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
+    ) {
+      tempcontract = contract.filter((c) => c.rental_frequency === "Monthly");
+      tempcontract.forEach((c) => {
+        Invoice.create({
+          tenant_id: c.tenant_id,
+          status: "Unpaid",
+          payment_for: "Rent",
+          amount_to_paid:
+            c.rental_amount *
+            new Date(
+              new Date().getFullYear(),
+              new Date().getMonth() + 1,
+              0
+            ).getDate(),
+          due_date: new Date(),
+          description: "Rental Collection",
+          received: 0,
+          balance: c.rental_amount,
+        });
+      });
+    }
+    res.send(200).json({ success: true, msg: "Reccuring Invoice Posted" });
+  } catch (error) {
+    res
+      .send(400)
+      .json({ success: false, msg: "Something in the server went wrong" });
   }
 });
 
